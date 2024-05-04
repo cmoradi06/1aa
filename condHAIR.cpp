e65d4c85d909e7746d72a0d6fd2e3c6942b9dbf0
cmoradi06 <49164902+cmoradi06@users.noreply.github.com>
2024-05-04 17:57:39 -0500

    Update condHAIR.cpp

--- condHAIR.cpp
+++ condHAIR.cpp
@@ -14,15 +14,15 @@ void condHAIR(int randZ) {
   if (randA <= 50)
     std::cout << "you got a mohawk haircut";
     else 
-      if (randA <= 70)
+      if (randA <= 75)
       std::cout << "you got a overgrown haircut";
-      else if (randA <= 80)
+      else if (randA <= 90)
         std::cout << "you got a buzz haircut";
-        else if (randA <= 90)
+        else if (randA <= 96)
           std::cout << "you got a bald haircut";
-          else if (randA <= 95)
-            std::cout << "you got a popstar haircut";
-            else
+          else if (randA <= 99)
             std::cout << "you got a bob haircut";
+            else
+            std::cout << "you got a popstar haircut";
 
 } 
